7a5c5936c6d32bf8d7f12357a92bf1e73587ddff
Your Name <you@example.com>
2018-09-29 15:14:42 -0700

    added html to the site

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,3 +19,11 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.urls import path, include
+from django.contrib import admin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+
+]
